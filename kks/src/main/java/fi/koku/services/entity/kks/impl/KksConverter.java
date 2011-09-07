4e12381d79a41d4977a1c5f1c120d61806133a97
Petri Tuomaala <petri.tuomaala@ixonos.com>
2011-09-07 12:20:02 +0000

    first draft of KKS service

---
+++ kks/src/main/java/fi/koku/services/entity/kks/impl/KksConverter.java
@@ -0,0 +1,14 @@
+package fi.koku.services.entity.kks.impl;
+
+/**
+ * Converts classes from DB to WS types and vice versa
+ * 
+ * @author tuomape
+ *
+ */
+public class KksConverter {
+  
+  
+  
+
+}
