307d7f9560111182b417502c34bfcd706a08f0d5
Petri Mikkonen <petri.mikkonen@ixonos.com>
2011-09-08 13:12:24 +0000

    Added Mock version of Kahva service

---
+++ kahvaservice-mock/src/main/java/fi/koku/services/entity/kahva/impl/KahvaServiceEndpointBean.java
@@ -0,0 +1,121 @@
+package fi.koku.services.entity.kahva.impl;
+
+import java.util.List;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fi.arcusys.tampere.hrsoa.entity.Organization;
+import fi.arcusys.tampere.hrsoa.entity.User;
+import fi.arcusys.tampere.hrsoa.ws.ldap.LdapService;
+
+
+
+/**
+ * Kahva service MOCK implementation class.
+ * 
+ * @author Ixonos / mikkope
+ */
+@Stateless
+@WebService(wsdlLocation="META-INF/wsdl/ldapService.wsdl",
+    endpointInterface="fi.arcusys.tampere.hrsoa.ws.ldap.LdapService",
+    targetNamespace="http://www.arcusys.fi/tampere/hrsoa/ws/ldap",
+    portName="ldapServiceSOAP",
+    serviceName="ldapService"
+)
+@RolesAllowed("koku-role")
+public class KahvaServiceEndpointBean implements LdapService {
+  private final Logger log = LoggerFactory.getLogger(KahvaServiceEndpointBean.class);
+
+  @Override
+  public List<User> getEmployees() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getEmployeesById(String userId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getEmployeesByOrganization(String organizationId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Organization getOrganizationById(String organizationId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getOrganizationEmployees(String employeeId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<Organization> getOrganizations() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getSuperiorsByEmployeeId(String employeeId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getSuperiorsById(String userId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getSuperiorsByOrganization(String organizationId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getSuperiorsBySSN(String ssn) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public User getUserByEmployeeId(String employeeId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public User getUserById(String userId) {
+    log.info("HALOOOOO: GetUserByID called with userid="+userId);
+    User u = new User();
+    u.setSsn("123456-1234");
+    return u;
+  }
+
+  @Override
+  public User getUserBySSN(String ssn) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<User> getUsersByIds(List<String> userId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+ 
+}
