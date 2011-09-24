ef600c7631024b45e022bdab4ab8932d39707bf2
Susanna Mäkinen <susanna.makinen@ixonos.com>
2011-09-24 10:38:18 +0000

    LogServiceBean-luokka

--- lok/src/main/java/fi/koku/services/utility/log/impl/LogServiceBean.java
+++ lok/src/main/java/fi/koku/services/utility/log/impl/LogServiceBean.java
@@ -1,25 +1,37 @@
 package fi.koku.services.utility.log.impl;
 
-import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+/**
+ * Log service implementation class
+ * 
+ * @author makinsu
+ *
+ */
 @Stateless
-public class LogServiceBean implements LogService {
+public class LogServiceBean implements LogService{
 
+  @EJB
+  LogDAO logDAO;
+  
   @Override
   public void archive(Date date) {
+    logDAO.archiveLog(date);
   }
 
   @Override
   public void write(LogEntry entry) {
+    logDAO.writeLog(entry);
   }
 
   @Override
   public List<LogEntry> query(LogQueryCriteria criteria) {
-    return Collections.emptyList();
+    return (List<LogEntry>) logDAO.queryLog(criteria);
   }
 
+  
 }
