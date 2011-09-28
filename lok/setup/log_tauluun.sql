ff203c78c358276caed3f0bc7f58e3d0276adcd1
Susanna Mäkinen <susanna.makinen@ixonos.com>
2011-09-28 12:20:45 +0000

    Some data for the log table.

---
+++ lok/setup/log_tauluun.sql
@@ -0,0 +1,12 @@
+insert into log (data_item_id,  timestamp, user_pic, customer_pic, data_item_type,
+	operation, client_system_id, message) VALUES ('123', '2010-05-06 13:03:22', '110166-111A', '030303A022T', 
+	'kks.vasu', 'write', 'kks', 'message123');
+	insert into log (data_item_id,  timestamp, user_pic, customer_pic, data_item_type,
+	operation, client_system_id, message) VALUES ('221', '2010-11-24 14:12:11', '110166-111A', '030303A022T', 
+	'kks.vasu', 'write', 'kks', 'viesti');
+	insert into log (data_item_id,  timestamp, user_pic, customer_pic, data_item_type,
+	operation, client_system_id, message) VALUES ('222', '2011-01-05 09:12:00', '110166-111A', '030303A022T', 
+	'kks.vasu', 'write', 'kks', 'vasun muutos');
+	insert into log (data_item_id,  timestamp, user_pic, customer_pic, data_item_type,
+	operation, client_system_id, message) VALUES ('351', '2011-01-08 12:12:09', '220166-2222', '030303A022T', 
+	'pyh.perhetiedot', 'write', 'pyh', 'perhetietojen lisäys');
