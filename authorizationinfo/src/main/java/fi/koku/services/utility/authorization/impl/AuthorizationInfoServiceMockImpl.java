5536f471db82b2100ec980f5e7e3fe8d4e9a190c
Petri Tuomaala <petri.tuomaala@ixonos.com>
2011-10-18 12:45:59 +0000

    added more mock users

--- authorizationinfo/src/main/java/fi/koku/services/utility/authorization/impl/AuthorizationInfoServiceMockImpl.java
+++ authorizationinfo/src/main/java/fi/koku/services/utility/authorization/impl/AuthorizationInfoServiceMockImpl.java
@@ -16,7 +16,6 @@ import fi.koku.services.utility.authorization.v1.GroupType;
 import fi.koku.services.utility.authorization.v1.GroupsType;
 import fi.koku.services.utility.authorization.v1.MemberPicsType;
 
-
 public class AuthorizationInfoServiceMockImpl {
   private Logger logger = LoggerFactory.getLogger(AuthorizationInfoServiceMockImpl.class);
 
@@ -24,7 +23,7 @@ public class AuthorizationInfoServiceMockImpl {
   private Map<String, List<GroupType>> roles = null;
   private Map<String, List<GroupType>> units = null;
   private Map<String, List<GroupType>> groups = null;
-  
+
   public AuthorizationInfoServiceMockImpl() {
     registries = initRegistries();
     roles = initRoles();
@@ -35,30 +34,31 @@ public class AuthorizationInfoServiceMockImpl {
   public GroupsType getGroups(GroupQueryCriteriaType gqc) {
     GroupsType res = new GroupsType();
     List<GroupType> groupsList = new ArrayList<GroupType>();
-    
+
     String grpClass = gqc.getGroupClass();
     String uid = gqc.getMemberPics().getMemberPic().get(0);
-    if("registry".equals(grpClass)) {
+    if ("registry".equals(grpClass)) {
       groupsList.addAll(getGroups(registries, uid));
-    } else if("role".equals(grpClass)) {
+    } else if ("role".equals(grpClass)) {
       groupsList.addAll(getGroups(roles, uid));
-    } else if("unit".equals(grpClass)) {
+    } else if ("unit".equals(grpClass)) {
       groupsList.addAll(getGroups(units, uid));
       groupsList.add(createGroup("unit", "kk.servicearea.childHealth", "Health care unit of Porolahti", uid));
-    } else if("group".equals(grpClass)) {
+    } else if ("group".equals(grpClass)) {
       groupsList.addAll(getGroups(groups, uid));
-      groupsList.add(createGroup("group", "gid1", "City region wide day care workgroup - consists of people from serveral organization units", uid));
+      groupsList.add(createGroup("group", "gid1",
+          "City region wide day care workgroup - consists of people from serveral organization units", uid));
     } else {
       throw new KoKuFaultException(3001, "unsupported group type");
     }
-    
+
     res.getGroup().addAll(groupsList);
     return res;
   }
-  
+
   private List<GroupType> getGroups(Map<String, List<GroupType>> groupMap, String key) {
     List<GroupType> r = groupMap.get(key);
-    if(r == null)
+    if (r == null)
       r = Collections.emptyList();
     return r;
   }
@@ -66,6 +66,46 @@ public class AuthorizationInfoServiceMockImpl {
   private Map<String, List<GroupType>> initRegistries() {
     Map<String, List<GroupType>> r = new HashMap<String, List<GroupType>>();
     String groupClass = "registry";
+
+    //healthcare
+    r.put("101010-1001", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1001")));
+    r.put("101010-1002", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1002")));
+    r.put("101010-1003", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1003")));
+    r.put("101010-1004", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1004")));
+    r.put("101010-1005", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1005")));
+    r.put("101010-1006", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1006")));
+    r.put("101010-1007", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1007")));
+    r.put("101010-1008", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1008")));
+    r.put("101010-1009", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1009")));
+    r.put("101010-1010", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1010")));
+    r.put("101010-1011", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1011")));
+    r.put("101010-1012", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1012")));
+    r.put("101010-1013", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1013")));
+    r.put("101010-1014", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1014")));
+    r.put("101010-1015", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1015")));
+    r.put("101010-1016", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1016")));
+    r.put("101010-1017", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1017")));
+    r.put("101010-1018", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1018")));
+    r.put("101010-1019", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1019")));
+    r.put("101010-1020", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "101010-1020")));
+    
+    //daycare
+    r.put("202020-2001", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2001")));
+    r.put("202020-2002", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2002")));
+    r.put("202020-2003", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2003")));
+    r.put("202020-2004", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2004")));
+    r.put("202020-2005", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2005")));
+    r.put("202020-2006", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2006")));
+    r.put("202020-2007", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2007")));
+    r.put("202020-2008", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2008")));
+    r.put("202020-2009", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2009")));
+    r.put("202020-2010", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2010")));
+    r.put("202020-2011", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2011")));
+    r.put("202020-2012", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2012")));
+    r.put("202020-2013", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2013")));
+    r.put("202020-2014", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2014")));
+    r.put("202020-2015", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "202020-2015")));
+    
     r.put("777777-7777", Arrays.asList(createGroup(groupClass, "daycareregistry", "Paivahoidon rekisteri", "777777-7777")));
     r.put("888888-8888", Arrays.asList(createGroup(groupClass, "healthcareregistry", "Potilastietorekisteri", "888888-8888")));
     return r;
@@ -75,25 +115,71 @@ public class AuthorizationInfoServiceMockImpl {
     Map<String, List<GroupType>> r = new HashMap<String, List<GroupType>>();
     String groupClass = "role";
     r.put("101010-1010", Arrays.asList(createGroup(groupClass, "ROLE_LOK_ADMIN", "ROLE_LOK_ADMIN", "101010-1010")));
-    r.put("121212-1212", Arrays.asList(createGroup(groupClass, "ROLE_LOK_LOG_ADMIN", "ROLE_LOK_LOG_ADMIN", "121212-1212")));
+    r.put("121212-1212",
+        Arrays.asList(createGroup(groupClass, "ROLE_LOK_LOG_ADMIN", "ROLE_LOK_LOG_ADMIN", "121212-1212")));
     return r;
   }
 
   private Map<String, List<GroupType>> initUnits() {
     Map<String, List<GroupType>> r = new HashMap<String, List<GroupType>>();
     String groupClass = "unit";
-    r.put("777777-7777", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "777777-7777")));
-    r.put("888888-8888", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "888888-8888")));
+    
+    
+    //healthcare
+    r.put("101010-1001", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1001")));
+    r.put("101010-1002", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1002")));
+    r.put("101010-1003", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1003")));
+    r.put("101010-1004", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1004")));
+    r.put("101010-1005", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1005")));
+    r.put("101010-1006", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1006")));
+    r.put("101010-1007", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1007")));
+    r.put("101010-1008", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1008")));
+    r.put("101010-1009", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1009")));
+    r.put("101010-1010", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1010")));
+    r.put("101010-1011", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1011")));
+    r.put("101010-1012", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1012")));
+    r.put("101010-1013", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1013")));
+    r.put("101010-1014", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1014")));
+    r.put("101010-1015", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1015")));
+    r.put("101010-1016", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1016")));
+    r.put("101010-1017", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1017")));
+    r.put("101010-1018", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1018")));
+    r.put("101010-1019", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1019")));
+    r.put("101010-1020", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth", "Health care unit of Porolahti", "101010-1020")));
+   
+    
+    //daycare
+    r.put("202020-2001", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2001")));
+    r.put("202020-2002", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2002")));
+    r.put("202020-2003", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2003")));
+    r.put("202020-2004", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2004")));
+    r.put("202020-2005", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2005")));
+    r.put("202020-2006", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2006")));
+    r.put("202020-2007", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2007")));
+    r.put("202020-2008", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2008")));
+    r.put("202020-2009", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2009")));
+    r.put("202020-2010", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2010")));
+    r.put("202020-2011", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2011")));
+    r.put("202020-2012", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2012")));
+    r.put("202020-2013", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2013")));
+    r.put("202020-2014", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2014")));
+    r.put("202020-2015", Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "202020-2015")));
+      
+    
+    r.put("777777-7777",
+        Arrays.asList(createGroup(groupClass, "kk.servicearea.daycare", "Day care unit of Porolahti", "777777-7777")));
+    r.put("888888-8888", Arrays.asList(createGroup(groupClass, "kk.servicearea.childHealth",
+        "Health care unit of Porolahti", "888888-8888")));
     return r;
   }
 
   private Map<String, List<GroupType>> initGroups() {
     Map<String, List<GroupType>> r = new HashMap<String, List<GroupType>>();
     String groupClass = "group";
-    
+
     return r;
   }
-  
+
   private GroupType createGroup(String groupClass, String id, String name, String memberUid) {
     GroupType g = new GroupType();
     g.setClazz(groupClass);
@@ -103,5 +189,5 @@ public class AuthorizationInfoServiceMockImpl {
     g.getMemberPics().getMemberPic().add(memberUid);
     return g;
   }
-  
+
 }
