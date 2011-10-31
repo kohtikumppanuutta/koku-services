deb7fe478bb36981e308ff55de350b1648fb55ad
marko asplund <marko.asplund@ixonos.com>
2011-10-31 10:11:41 +0000

    KOKU-656: add file header

--- kks/src/main/java/fi/koku/services/entity/kks/impl/KksEntryClass.java
+++ kks/src/main/java/fi/koku/services/entity/kks/impl/KksEntryClass.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * 
+ * You should have received a copy of the license text along with this program.
+ * If not, please contact the copyright holder (http://www.ixonos.com/).
+ * 
+ */
 package fi.koku.services.entity.kks.impl;
 
 import java.io.Serializable;
