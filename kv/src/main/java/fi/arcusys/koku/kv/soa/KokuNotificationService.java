7e4cbedaa4896ad460584cc4b067e049c999dbd7
Dmitry Kudinov <dmitry.kudinov@arcusys.fi>
2011-12-20 14:18:48 +0000

    added web service endpoint for Notification service

---
+++ kv/src/main/java/fi/arcusys/koku/kv/soa/KokuNotificationService.java
@@ -0,0 +1,18 @@
+package fi.arcusys.koku.kv.soa;
+
+import java.util.List;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+/**
+ * @author Dmitry Kudinov (dmitry.kudinov@arcusys.fi)
+ * Dec 20, 2011
+ */
+@WebService(targetNamespace = "http://soa.kv.koku.arcusys.fi/")
+public interface KokuNotificationService {
+    void sendNotification(
+            @WebParam(name = "subject") final String subject, 
+            @WebParam(name = "recipients") final List<String> recipients, 
+            @WebParam(name = "content") final String content);
+}
---
+++ kv/src/main/java/fi/arcusys/koku/kv/soa/KokuNotificationServiceImpl.java
@@ -0,0 +1,35 @@
+package fi.arcusys.koku.kv.soa;
+
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.jws.WebService;
+
+import fi.arcusys.koku.common.service.KokuSystemNotificationsService;
+
+/**
+ * @author Dmitry Kudinov (dmitry.kudinov@arcusys.fi)
+ * Dec 20, 2011
+ */
+@Stateless
+@WebService(serviceName = "KokuNotificationService", portName = "KokuNotificationServicePort", 
+        endpointInterface = "fi.arcusys.koku.kv.soa.KokuNotificationService",
+        targetNamespace = "http://soa.kv.koku.arcusys.fi/")
+public class KokuNotificationServiceImpl implements KokuNotificationService {
+
+    @EJB
+    private KokuSystemNotificationsService notificationService;
+
+    /**
+     * @param subject
+     * @param receipients
+     * @param content
+     */
+    @Override
+    public void sendNotification(String subject, List<String> receipients,
+            String content) {
+        notificationService.sendNotification(subject, receipients, content);
+    }
+    
+}
