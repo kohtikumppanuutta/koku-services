2808a0e262b1484edbbad0771ec5f8251bc97af2
Dmitry Kudinov <dmitry.kudinov@arcusys.fi>
2011-12-27 10:48:03 +0000

    minor correction for reminders about requests - duplicate reminder removed

--- arcusys-common/src/main/java/fi/arcusys/koku/common/service/impl/RequestDAOImpl.java
+++ arcusys-common/src/main/java/fi/arcusys/koku/common/service/impl/RequestDAOImpl.java
@@ -124,6 +124,7 @@ public class RequestDAOImpl extends AbstractEntityDAOImpl<Request> implements Re
         final Calendar calendar = CalendarUtil.getXmlDate(time).toGregorianCalendar();
         params.put("notifyDateFrom", calendar.getTime());
         calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) + 1);
+        calendar.set(Calendar.SECOND, calendar.get(Calendar.SECOND) - 1);
         params.put("notifyDateTo", calendar.getTime());
         return getResultList("findOpenRequestsByNotificationDate", params);
     }
