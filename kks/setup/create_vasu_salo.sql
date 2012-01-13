dbfd0d2f38cd48acfc18790a74c7ed91d010df45
Petri Tuomaala <petri.tuomaala@ixonos.com>
2012-01-13 13:29:54 +0000

    salon vasu

---
+++ kks/setup/create_vasu_salo.sql
@@ -0,0 +1,790 @@
+insert into kks_collection_class (
+  id
+  ,type_code
+  ,name
+  ,description
+  ,consent_type
+) VALUES (
+	60
+  ,'kks.kehitysasialaji.vasu.salo'   -- type_code
+  ,'Varhaiskasvatussuunnitelma (Salo)'  -- name
+  ,''  -- description
+  ,''  -- concent_type
+);
+
+insert into kks_group (
+  group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,collection_class_id
+) VALUES (
+   550   -- group_id
+  ,1   -- sort_order
+  ,'Huoltaja'  -- name
+  ,''  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,60
+);
+
+insert into kks_group (
+  group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,collection_class_id
+) VALUES (
+   551   -- group_id
+  ,2   -- sort_order
+  ,'Päivähoito'  -- name
+  ,''  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,60
+);
+
+insert into kks_group (
+   group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,parent_id
+  ,collection_class_id
+) VALUES (
+   552   -- group_id
+  ,1   -- sort_order
+  ,'Elinolosuhteet'  -- name
+  ,'Asiat, jotka vaikuttavat päivittäiseen kasvatuskumppanuuteen'  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,550   -- parent_id
+  ,60
+);
+
+insert into kks_group (
+   group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,parent_id
+  ,collection_class_id
+) VALUES (
+   553   -- group_id
+  ,2   -- sort_order
+  ,'Lapsen hyvinvointi'  -- name
+  ,''  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,550   -- parent_id
+  ,60
+);
+
+insert into kks_group (
+   group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,parent_id
+  ,collection_class_id
+) VALUES (
+   554   -- group_id
+  ,3   -- sort_order
+  ,'Mediakasvatus'  -- name
+  ,''  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,550   -- parent_id
+  ,60
+);
+
+insert into kks_group (
+   group_id
+  ,sort_order
+  ,name
+  ,description
+  ,register
+  ,accountable
+  ,parent_id
+  ,collection_class_id
+) VALUES (
+   555   -- group_id
+  ,1   -- sort_order
+  ,'Tavoitteet'  -- name
+  ,''  -- description
+  ,'daycareregistry'  -- register
+  ,'guardian' -- accountable
+  ,551   -- parent_id
+  ,60
+);
+
+-- Huoltaja
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   600   -- entry_class_id
+  ,1   -- sort_order
+  ,'Lapsen äidinkieli'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,552   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   601   -- entry_class_id
+  ,2   -- sort_order
+  ,'Muut perheessä puhutu kielet'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,552   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   602   -- entry_class_id
+  ,3   -- sort_order
+  ,'Tapaamiset, jos vanhemmat asuvat erillään'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,552   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   603   -- entry_class_id
+  ,4   -- sort_order
+  ,'Mikäli vanhemmilla yhteishuoltajuus, voiko huoltajan avo/aviopuoliso osallistua vasu keskusteluun?'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'SELECTION'  -- data_type
+  ,'kyllä,ei'  -- value_spaces
+  ,552   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   604   -- entry_class_id
+  ,5   -- sort_order
+  ,'Muut mahdolliset asiat'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,552   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   605   -- entry_class_id
+  ,6   -- sort_order
+  ,'Nukkuminen ja päivälepo'  -- name
+  ,'Miten pitkät yöunet, herääminen, vireystila heräämisen jälkeen jne.'  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   606   -- entry_class_id
+  ,7   -- sort_order
+  ,'Ruokailutottumukset'  -- name
+  ,'Käyttääkö ruokailuvälineitä itsenäisesti, syökö siististi, ruokailutavat, mieliruoat ym.'  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   607   -- entry_class_id
+  ,8   -- sort_order
+  ,'WC-käytännöt ja hygienia'  -- name
+  ,'Vaippa, potta, wc-istuin, omatoimisuus, käsien peseminen'  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   608   -- entry_class_id
+  ,9   -- sort_order
+  ,'Omatoimisuus'  -- name
+  ,'Pukeutuminen, tavaroista huolehtiminen jne.'  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   609   -- entry_class_id
+  ,10   -- sort_order
+  ,'Muuta terveyteen liittyvää'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   610   -- entry_class_id
+  ,11   -- sort_order
+  ,'Toiveet tuomis- ja hakutilanteissa'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   611   -- entry_class_id
+  ,12   -- sort_order
+  ,'Huolenaiheiden esille ottaminen'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   612   -- entry_class_id
+  ,13   -- sort_order
+  ,'Toiveet ja odotukset päivähoidolle'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,553   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   613   -- entry_class_id
+  ,14   -- sort_order
+  ,'Onko teillä sääntöjä tv:n katselusta, videoista tai tietokonepeleistä?'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,554   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   614   -- entry_class_id
+  ,15   -- sort_order
+  ,'Katsotteko yhdessä lapsenne kanssa tv:tä?'  -- name
+  ,'Kuinka usein, mitä ohjelmia...'  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,554   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   615   -- entry_class_id
+  ,16   -- sort_order
+  ,'Mistä ohjelmista lapsesi pitää?'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,554   -- entry_group
+);
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   616   -- entry_class_id
+  ,17   -- sort_order
+  ,'Tuemme lapsen kehitystä seuraavalla tavalla'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,555   -- entry_group
+);
+
+-- Päivähoito
+
+insert into kks_entry_class (
+   entry_class_id
+  ,sort_order
+  ,name
+  ,description
+  ,multi_value
+  ,data_type
+  ,value_spaces
+  ,entry_group
+) VALUES (
+   617   -- entry_class_id
+  ,18   -- sort_order
+  ,'Arviointi'  -- name
+  ,''  -- description
+  ,0   -- multi_value
+  ,'FREE_TEXT'  -- data_type
+  ,''  -- value_spaces
+  ,555   -- entry_group
+);
+
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   600   -- entry_class_id
+  ,13   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   600   -- entry_class_id
+  ,25   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   600   -- entry_class_id
+  ,48   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   601   -- entry_class_id
+  ,13   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   601   -- entry_class_id
+  ,25   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   601   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   602   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   602   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   603   -- entry_class_id
+  ,13   -- tag_id
+);
+
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   603   -- entry_class_id
+  ,64   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   604   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   604   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   605   -- entry_class_id
+  ,18   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   605   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   606   -- entry_class_id
+  ,18   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   606   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   607   -- entry_class_id
+  ,18   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   607   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   608   -- entry_class_id
+  ,18   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   608   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   609   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   609   -- entry_class_id
+  ,41   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   610   -- entry_class_id
+  ,19   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   610   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   611   -- entry_class_id
+  ,19   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   611   -- entry_class_id
+  ,40   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   612   -- entry_class_id
+  ,19   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   612   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   613   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   613   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   613   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   614   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   614   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   614   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   615   -- entry_class_id
+  ,13   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   615   -- entry_class_id
+  ,33   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   615   -- entry_class_id
+  ,48   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   616   -- entry_class_id
+  ,15   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   616   -- entry_class_id
+  ,55   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   617   -- entry_class_id
+  ,11   -- tag_id
+);
+insert into kks_entry_class_tags (
+   entry_class_id
+  ,tag_id
+) VALUES (
+   617   -- entry_class_id
+  ,55   -- tag_id
+);
+
+
