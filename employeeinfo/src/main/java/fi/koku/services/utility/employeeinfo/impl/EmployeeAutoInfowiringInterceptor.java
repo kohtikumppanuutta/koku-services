273987eec246b337c3e1d447d00c44a9a6c76f80
marko asplund <marko.asplund@ixonos.com>
2012-01-17 14:44:43 +0000

    add getBeanFactoryLocator method

--- employeeinfo/src/main/java/fi/koku/services/utility/employeeinfo/impl/EmployeeAutoInfowiringInterceptor.java
+++ employeeinfo/src/main/java/fi/koku/services/utility/employeeinfo/impl/EmployeeAutoInfowiringInterceptor.java
@@ -1,5 +1,7 @@
 package fi.koku.services.utility.employeeinfo.impl;
 
+import org.springframework.beans.factory.access.BeanFactoryLocator;
+import org.springframework.context.access.ContextSingletonBeanFactoryLocator;
 import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
 
 /**
@@ -9,10 +11,17 @@ import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
  * 
  * @author hanhian
  */
-public class EmployeeAutoInfowiringInterceptor extends SpringBeanAutowiringInterceptor{
+public class EmployeeAutoInfowiringInterceptor extends SpringBeanAutowiringInterceptor {
+  private static final String BEAN_FACTORY_RESOURCE_LOCATION = "classpath*:beanRefContext-empinfo.xml";
   
   @Override
   protected String getBeanFactoryLocatorKey(Object target) {  
     return "employeeinfo-ejb-context";
-  }  
+  }
+
+//  @Override
+//  protected BeanFactoryLocator getBeanFactoryLocator(Object target) {
+//    return ContextSingletonBeanFactoryLocator.getInstance(BEAN_FACTORY_RESOURCE_LOCATION);
+//  }
+  
 }
