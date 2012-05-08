fe6b48babd799810d5300966e3548a9997941486
marko asplund <marko.asplund@ixonos.com>
2012-05-08 10:31:35 +0300

    KOKU-1284: update file header

--- lok/src/main/java/fi/koku/services/utility/log/impl/LogServiceEndpointBean.java
+++ lok/src/main/java/fi/koku/services/utility/log/impl/LogServiceEndpointBean.java
@@ -1,9 +1,13 @@
 /*
- * Copyright 2011 Ixonos Plc, Finland. All rights reserved.
+ * Copyright 2012 Ixonos Plc, Finland. All rights reserved.
  * 
- * You should have received a copy of the license text along with this program.
- * If not, please contact the copyright holder (http://www.ixonos.com/).
- * 
+ * This file is part of Kohti kumppanuutta.
+ *
+ * This file is licensed under GNU LGPL version 3.
+ * Please see the 'license.txt' file in the root directory of the package you received.
+ * If you did not receive a license, please contact the copyright holder
+ * (http://www.ixonos.com/).
+ *
  */
 package fi.koku.services.utility.log.impl;
 
